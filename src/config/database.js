69e3b6ec5611852dfd234a39a27616b9949fbdce
Kartikey Pande <71623412+kartikey151999@users.noreply.github.com>
2025-02-27 11:35:48 +0530

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -3,8 +3,4 @@ const mongoose = require("mongoose");
 const db = async () =>{
     await mongoose.connect("mongodb+srv://ankurpande:123Mongodb@cluster1.pimpp.mongodb.net/devtinder")
 }
-
-
-
-// 123Mongodb
 module.exports = db;
